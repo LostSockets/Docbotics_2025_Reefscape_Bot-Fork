3d6184ce0815db9e9017c149090423466c921fa2
CorpuzIhate <guyihate@gmail.com>
2025-01-14 23:27:45 -0500

    Absolute encoder is now based on the absolute encoder object in the revlib library. The Encoder is taken from the motor controller of the arm motor

--- src/main/java/frc/robot/commands/MoveArmCMD.java
+++ src/main/java/frc/robot/commands/MoveArmCMD.java
@@ -1,19 +1,23 @@
 package frc.robot.commands;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.subsystems.ArmSub;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class MoveArmCMD extends Command{
     public final ArmSub armSub; 
     public final SparkMax armMotor;
     public final PIDController armController;
+
 
     private final int setpoint = 300;
     
@@ -21,6 +25,7 @@ public class MoveArmCMD extends Command{
         this.armSub = armSub;
         armController = armSub.getPIDController();
         armMotor = armSub.getMotor();
+    
         addRequirements(armSub);
         
     }
@@ -37,9 +42,9 @@ public class MoveArmCMD extends Command{
     
     @Override
     public void execute(){
-        // double output = armController.calculate(armSub.getGetArmEncoder().getAbsolutePosition(), setpoint);
-        // SmartDashboard.putNumber("armPosition_ticks", armSub.getGetArmEncoder().getAbsolutePosition());
-        // SmartDashboard.putNumber("armPowerOutput_PercentOfPower",output);
+     double output = armController.calculate(armSub.getGetArmEncoder().getPosition(), setpoint);
+         SmartDashboard.putNumber("armPosition_rotation", armSub.getGetArmEncoder().getPosition());
+        SmartDashboard.putNumber("armPowerOutput_PercentOfPower",output);
         armSub.setArmSpeed(1);
         
     }
--- src/main/java/frc/robot/subsystems/ArmSub.java
+++ src/main/java/frc/robot/subsystems/ArmSub.java
@@ -2,13 +2,14 @@
 package frc.robot.subsystems;
 
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.google.flatbuffers.Constants;
 
 
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.DutyCycle;
+
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
@@ -19,18 +20,20 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 public class ArmSub extends SubsystemBase{
     private SparkMax armMotor = new SparkMax(ArmConstants.kArmMotorPort, MotorType.kBrushless);
     private DutyCycleEncoder m_armEncoder = new DutyCycleEncoder(0);
-     ;
+    
     private PIDController armController = new PIDController(
         ArmConstants.kP, 
         ArmConstants.kI, 
         ArmConstants.kD);
+    
 
 
     public SparkMax getMotor(){
         return armMotor;
     }
-        public DutyCycleEncoder getGetArmEncoder(){
-        return m_armEncoder;
+        public AbsoluteEncoder getGetArmEncoder(){
+
+        return armMotor.getAbsoluteEncoder();
     }
     public PIDController getPIDController(){
         return armController;
