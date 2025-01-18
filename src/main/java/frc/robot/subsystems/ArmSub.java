28e998aa446096bee8eacc47e87b9fd4e0fc6913
CorpuzIhate <guyihate@gmail.com>
2025-01-18 00:45:03 -0500

    configured the arm motor to use the Spark Max data port

--- src/main/java/frc/robot/subsystems/ArmSub.java
+++ src/main/java/frc/robot/subsystems/ArmSub.java
@@ -4,35 +4,45 @@ package frc.robot.subsystems;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
-import com.google.flatbuffers.Constants;
+
 
 
 
 import edu.wpi.first.math.controller.PIDController;
-
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 
 
 public class ArmSub extends SubsystemBase{
+    private SparkBaseConfig armMotorConfig;
+    private DutyCycleEncoder armEncoder;
     private SparkMax armMotor = new SparkMax(ArmConstants.kArmMotorPort, MotorType.kBrushless);
-    private DutyCycleEncoder m_armEncoder = new DutyCycleEncoder(0);
+
     
     private PIDController armController = new PIDController(
         ArmConstants.kP, 
         ArmConstants.kI, 
         ArmConstants.kD);
+
+    public ArmSub(){
+        armMotorConfig.absoluteEncoder.setSparkMaxDataPortConfig();
+        armMotor.configure(armMotorConfig, ResetMode.kResetSafeParameters, 
+        PersistMode.kNoPersistParameters);
+    }
     
 
 
     public SparkMax getMotor(){
         return armMotor;
     }
-        public AbsoluteEncoder getGetArmEncoder(){
 
+    public AbsoluteEncoder getGetArmEncoder(){
         return armMotor.getAbsoluteEncoder();
     }
     public PIDController getPIDController(){
