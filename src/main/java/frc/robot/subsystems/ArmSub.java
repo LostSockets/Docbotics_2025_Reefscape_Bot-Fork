86672fa8859c07fcc81d4c8ea9540e4aae02b2c5
CorpuzIhate <guyihate@gmail.com>
2025-01-22 12:30:02 -0500

    Revert "Merge branch 'dev-ArmMotorTest'"
    This reverts commit 99266f05b13f0897147324a02c9169f1ca91d481, reversing
    changes made to 02b44d31730d6ed017981574c7f4fd6d71f54e9b.

--- src/main/java/frc/robot/subsystems/ArmSub.java
+++ src/main/java/frc/robot/subsystems/ArmSub.java
@@ -2,64 +2,39 @@
 package frc.robot.subsystems;
 
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
-
+import com.google.flatbuffers.Constants;
 
 
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
 
 
 public class ArmSub extends SubsystemBase{
-    private SparkMaxConfig armMotorConfig = new SparkMaxConfig();
     private SparkMax armMotor = new SparkMax(ArmConstants.kArmMotorPort, MotorType.kBrushless);
-
-    
-    public ArmSub(){
-        //configures armMotors PID Constants, min and max power outputs,
-        //max veloctiy and acceleration
-        armMotorConfig
-        .closedLoop
-        .p(0.0005)
-        .i(0)
-        .d(0)
-        .maxOutput(1)
-        .minOutput(-1)
-        .maxMotion
-            .maxVelocity(10000)
-            .maxAcceleration(360)
-            .allowedClosedLoopError(1);
-
-
-        armMotor.configure(armMotorConfig,
-        ResetMode.kResetSafeParameters, 
-        PersistMode.kNoPersistParameters);
-        
-    }
-
-
+    private DutyCycleEncoder m_armEncoder = new DutyCycleEncoder(0);
+     ;
+    private PIDController armController = new PIDController(
+        ArmConstants.kP, 
+        ArmConstants.kI, 
+        ArmConstants.kD);
 
 
     public SparkMax getMotor(){
         return armMotor;
     }
-
-    public AbsoluteEncoder getGetArmEncoder(){
-        return armMotor.getAbsoluteEncoder();
+        public DutyCycleEncoder getGetArmEncoder(){
+        return m_armEncoder;
     }
-
+    public PIDController getPIDController(){
+        return armController;
+    }
     
     public void setArmSpeed(double speed){
         armMotor.set(speed);
