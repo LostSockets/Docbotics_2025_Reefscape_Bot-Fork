6ba0b9b4599a7a5c1005c8822502c779a334329e
CorpuzIhate <guyihate@gmail.com>
2025-02-04 14:06:23 -0500

    created elevator subsystem and commands

---
+++ src/main/java/frc/robot/commands/MoveElevatorToSetpointCMD.java
@@ -0,0 +1,76 @@
+package frc.robot.commands;
+
+import com.revrobotics.RelativeEncoder;
+
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.ArmSub;
+import frc.robot.subsystems.ElevatorSub;
+
+public class MoveElevatorToSetpointCMD extends Command {
+    public final ElevatorSub elevatorSub;
+    public final SparkMax primaryLeftElevatorMotor;
+    public final SparkMax rightElevatorMotor;
+    public final PIDController elevatorController;
+
+    public MoveElevatorToSetpointCMD(ElevatorSub elevatorSub) {
+        this.elevatorSub = elevatorSub;
+        this.primaryLeftElevatorMotor = elevatorSub.getPrimaryLeftElevatorMotor();
+        this.rightElevatorMotor = elevatorSub.getRightElevatorMotor();
+        elevatorController = elevatorSub.getElevatorController();
+        addRequirements(elevatorSub);
+
+    }
+
+    @Override
+    public void initialize() {
+
+        /**
+         * When command starts, stop all elevator motors.
+         * Right elevator motor will always FOLLOW
+         * the left elevator motor in the OPPOSITE,.
+         */
+        primaryLeftElevatorMotor.set(0);
+        primaryLeftElevatorMotor.stopMotor();
+
+        SmartDashboard.putBoolean("isElevatorCommandRunning", true);
+    }
+
+    @Override
+    public void execute() {
+        /* Send elevator telemetry */
+        SmartDashboard.putData(elevatorController);
+        SmartDashboard.putNumber("elevatorPositionError_meters", elevatorController.getError());
+        SmartDashboard.putNumber("elevatorPosition_meters", elevatorSub.getPrimaryElevatorPosition());
+        // Drive elevator Motor to set-point based on elevator controller.
+        double output = elevatorController.calculate(elevatorSub.getPrimaryElevatorPosition(), 0.5);
+        primaryLeftElevatorMotor.set(output);
+    }
+
+    /**
+     * When elevator command ends
+     * stop all motors;
+     */
+    @Override
+    public void end(boolean interrupted) {
+        SmartDashboard.putBoolean("isElevatorCommandRunning", false);
+        primaryLeftElevatorMotor.set(0);
+        primaryLeftElevatorMotor.stopMotor();
+    }
+
+    @Override
+    public boolean isFinished() {
+        /*
+         * When current elevator position is less than 0.5m away
+         * end the command.
+         */
+        if (Math.abs(elevatorController.getError()) <= 0.2) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/ElevatorSub.java
@@ -0,0 +1,77 @@
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.google.flatbuffers.Constants;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DutyCycle;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.ElevatorConstants;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class ElevatorSub extends SubsystemBase {
+    /* Elevator motors. */
+    private SparkMax primaryLeftElevatorMotor = new SparkMax(ElevatorConstants.kLeftElevatorMotorPort,
+            MotorType.kBrushless);
+    private SparkMax rightElevatorMotor = new SparkMax(ElevatorConstants.kRightElevatorMotorPort, MotorType.kBrushless);
+    /* Elevator motor configurations. */
+    private SparkMaxConfig rightElevatorMotorConfig = new SparkMaxConfig();
+    private SparkMaxConfig leftElevatorMotorConfig = new SparkMaxConfig();
+
+    /** PID controller to move the elevator move to position. */
+    private PIDController elevatorPIDController = new PIDController(
+            ElevatorConstants.kP,
+            ElevatorConstants.kI,
+            ElevatorConstants.kD);
+
+    public ElevatorSub() {
+        /**
+         * Right elevator motor will follow the
+         * left motor and be inverted.
+         */
+        rightElevatorMotorConfig
+                .follow(ElevatorConstants.kLeftElevatorMotorPort, true);
+        /**
+         * Convert primary elevator motor encoder ticks to
+         * displacement inn elevator height in meters.
+         */
+        leftElevatorMotorConfig.encoder.positionConversionFactor(ElevatorConstants.elevatorMotorRotationToMeters);
+
+        /**
+         * apply right and left elevator motor config. Will only change new parameters.
+         * parameters do no persist across power cycles.
+         */
+        rightElevatorMotor.configure(rightElevatorMotorConfig, ResetMode.kResetSafeParameters,
+                PersistMode.kNoPersistParameters);
+
+    }
+
+    /** @return the left elevator Motor. */
+    public SparkMax getPrimaryLeftElevatorMotor() {
+        return primaryLeftElevatorMotor;
+    }
+
+    /** @return the right elevator Motor. */
+    public SparkMax getRightElevatorMotor() {
+        return rightElevatorMotor;
+    }
+
+    /** @return primary elevator Motor position in meters. */
+    public double getPrimaryElevatorPosition() {
+        return primaryLeftElevatorMotor.getEncoder().getPosition()
+                + ElevatorConstants.initialHeightOfElevatorToGround_Meters;
+    }
+
+    /** @return the elevator PID Controller */
+    public PIDController getElevatorController() {
+        return elevatorPIDController;
+    }
+
+}
