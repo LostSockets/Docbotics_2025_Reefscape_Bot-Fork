08731f0281b551080dbae64f695314a5142531c8
CorpuzIhate <guyihate@gmail.com>
2025-02-27 17:25:11 -0500

    Started adding reset swerve modules command for auto.

---
+++ src/main/java/frc/robot/autoCommands/resetSwerveModuleSpeedsCMD.java
@@ -0,0 +1,45 @@
+package frc.robot.autoCommands;
+
+import java.util.function.Supplier;
+
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralIntakeConsumerSub;
+import frc.robot.subsystems.CoralPitcherIntakeSub;
+import frc.robot.subsystems.SwerveSub;
+
+public class resetSwerveModuleSpeedsCMD extends Command {
+    private final SwerveSub swerveSub;
+
+
+
+    public resetSwerveModuleSpeedsCMD(
+        SwerveSub swerveSub ) {
+        this.swerveSub = swerveSub;
+
+        addRequirements(swerveSub);
+
+    }
+
+    @Override
+    public void initialize() {
+
+      
+    }
+
+    @Override
+    public void execute() {}
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return false;
+    }
+}
