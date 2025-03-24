72c1dd42391e37847513c969d3ab9bd4163c6e78
MrCauthers <tv40670@tvdsb.ca>
2025-03-24 15:00:12 -0400

    changed camera resolution

--- src/main/java/frc/robot/subsystems/SwerveSub.java
+++ src/main/java/frc/robot/subsystems/SwerveSub.java
@@ -105,7 +105,7 @@ public class SwerveSub extends SubsystemBase {
 
     public SwerveSub(){
 
-        CameraServer.startAutomaticCapture();
+        CameraServer.startAutomaticCapture().setResolution(320,400);
 
  new Thread(() -> {  /// try catch function is a fancy if else statement
         try{              // it tries to run a thread of resseting the gryo but if it exception e happens it stops 
